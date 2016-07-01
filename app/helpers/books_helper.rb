e1d86b65379a7eeb0ce5768c28a05f081c8ea147
Kong <kxrules@gmail.com>
2016-07-01 14:43:59 -0500

    fixed a couple bugs with the view and added image preview new and edit books

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,8 @@
 module BooksHelper
+	def form_image_select(book)
+		return image_tag book.book_img.url(:medium),
+	               id: 'image-preview',
+	               class: 'img-responsive' if book.book_img.exists?
+	           	image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+	end
 end
